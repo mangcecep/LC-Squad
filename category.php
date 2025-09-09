b8b097dd49ca161b1de4a806129d2ca98433226b
syiffa352 <syiffaindahskira@gmail.com>
2025-09-09 12:36:02 +0700

    nambah category

---
+++ category.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Category</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body style="background-color: lightblue;">
+
+    <!-- Container Page Item -->
+
+    <!-- Navbar -->
+    <div class="bg-blue-800 text-white flex justify-between items-center px-6 py-3">
+        <div class="flex items-center gap-2">
+            <div class="bg-white text-blue-800 p-2 rounded">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
+                    <path fill="005597Color"
+                        d="m17.578 4.432l-2-1.05C13.822 2.461 12.944 2 12 2s-1.822.46-3.578 1.382l-.321.169l8.923 5.099l4.016-2.01c-.646-.732-1.688-1.279-3.462-2.21m4.17 3.534l-3.998 2V13a.75.75 0 0 1-1.5 0v-2.286l-3.5 1.75v9.44c.718-.179 1.535-.607 2.828-1.286l2-1.05c2.151-1.129 3.227-1.693 3.825-2.708c.597-1.014.597-2.277.597-4.8v-.117c0-1.893 0-3.076-.252-3.978M11.25 21.904v-9.44l-8.998-4.5C2 8.866 2 10.05 2 11.941v.117c0 2.525 0 3.788.597 4.802c.598 1.015 1.674 1.58 3.825 2.709l2 1.049c1.293.679 2.11 1.107 2.828 1.286M2.96 6.641l9.04 4.52l3.411-1.705l-8.886-5.078l-.103.054c-1.773.93-2.816 1.477-3.462 2.21" />
+                </svg>
+            </div>
+            <h1 class="flex items-center gap-2">BARANG</h1>
+        </div>
+        <div class="flex items-center gap-2">
+            <span>ADMIN</span>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 512 512">
+                    <path fill="currentColor" fill-rule="evenodd"
+                        d="M256 42.667A213.333 213.333 0 0 1 469.334 256c0 117.821-95.513 213.334-213.334 213.334c-117.82 0-213.333-95.513-213.333-213.334C42.667 138.18 138.18 42.667 256 42.667m21.334 234.667h-42.667c-52.815 0-98.158 31.987-117.715 77.648c30.944 43.391 81.692 71.685 139.048 71.685s108.104-28.294 139.049-71.688c-19.557-45.658-64.9-77.645-117.715-77.645M256 106.667c-35.346 0-64 28.654-64 64s28.654 64 64 64s64-28.654 64-64s-28.653-64-64-64" />
+                </svg>
+            </div>
+        </div>
+    </div>
+
+    <!-- Content -->
+    <div class="p-6">
+        <!-- Back Button -->
+        <button class="bg-white border px-3 py-1 rounded shadow flex items-center gap-2 mb-4 hover:bg-gray-100">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                <path fill="currentColor"
+                    d="M12 1c1.1 0 2 .895 2 2v9c0 1.1-.895 2-2 2H9.5a.5.5 0 0 1 0-1H12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H9.5a.5.5 0 0 1 0-1z" />
+                <path fill="currentColor"
+                    d="M4.15 4.15a.5.5 0 0 1 .707.707l-2.15 2.15h6.79a.5.5 0 0 1 0 1h-6.79l2.15 2.15a.5.5 0 0 1-.707.707l-3-3a.5.5 0 0 1 0-.707l3-3z" />
+            </svg>Back
+        </button>
+
+        <!-- Title --> 
+        <h2 class="text-center font-bold text-xl mb-6">Category</h2>
+
+        <!-- Grid Item -->
+        <div class="grid  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-12">
+            <div
+                class="bg-white w-[230px] h-[170px] shadow-lg grid items-center justify-center font-medium rounded hover:shadow-xl hover:-translate-y-1 transition pt-10" style="margin-left: 100px;">
+                Seragam</div>
+            <div
+                class="bg-white w-[230px] h-[170px] shadow-lg grid items-center justify-center font-medium rounded hover:shadow-xl hover:-translate-y-1 transition pt-10" style="margin-left: 100px;">
+                Toolkit</div>
+            <div
+                class="bg-white w-[230px] h-[170px] shadow-lg grid items-center justify-center font-medium rounded hover:shadow-xl hover:-translate-y-1 transition pt-10" style="margin-left: 100px;">
+                Seragam</div>
+            <div
+                class="bg-white w-[230px] h-[170px] shadow-lg grid items-center justify-center font-medium rounded hover:shadow-xl hover:-translate-y-1 transition pt-10" style="margin-left: 100px;">
+                Seragam</div>
+            <div
+                class="bg-white w-[230px] h-[170px] shadow-lg grid items-center justify-center font-medium rounded hover:shadow-xl hover:-translate-y-1 transition pt-10" style="margin-left: 100px;">
+                Seragam</div>
+            <div
+                class="bg-white w-[230px] h-[170px] shadow-lg grid items-center justify-center font-medium rounded hover:shadow-xl hover:-translate-y-1 transition pt-10" style="margin-left: 100px;">
+                Seragam</div>
+            <div
+                class="bg-white w-[230px] h-[170px] shadow-lg grid items-center justify-center font-medium rounded hover:shadow-xl hover:-translate-y-1 transition pt-10" style="margin-left: 100px;">
+                Seragam</div>
+            <div
+                class="bg-white w-[230px] h-[170px] shadow-lg grid items-center justify-center font-medium rounded hover:shadow-xl hover:-translate-y-1 transition pt-10" style="margin-left: 100px;">
+                Seragam</div>
+        </div>
+    </div>
+    </div>
+
+</body>
+
+</html>
